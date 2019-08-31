8c81109fc608833158bf6c0d197539467b0c82bb
leehosu <hosu4549@gmail.com>
2019-08-31 12:06:48 +0900

    First Commit and basic html setting

---
+++ todo/src/index.js
@@ -0,0 +1,7 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App';
+
+const destination = document.querySelector(".root");
+
+ReactDOM.render(<App />, destination);
